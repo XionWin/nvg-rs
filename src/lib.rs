21cb628fc5cf65280e02f280a76b9c2575eafbf0
XionWin <xionwin@hotmail.com>
2024-01-17 00:20:03 +0800

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 mod as_primitive;
 #[allow(dead_code)]
-mod math;
+pub(crate) mod math;
+mod color;
 
-pub(crate) use math::*;
+pub use color::*;
 
 
 pub fn test_func() {
-    let p = Point::from((3, 2));
+    let p = math::Point::from((3, 2));
     println!("point: {:#?}", p);
     panic!("test end");
 }
